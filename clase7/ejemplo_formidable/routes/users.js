616b9aa202a8102b559dc864ec4d03123d791fc1
luisNavasArg <lpipnavas@gmail.com>
2022-10-24 22:07:24 -0300

    Clase 8 Comenzamos el tpFinal

--- clase7/ejemplo_formidable/routes/users.js
+++ clase7/ejemplo_formidable/routes/users.js
@@ -3,11 +3,12 @@ var router = express.Router();
 const formidable = require('formidable');
 const fs = require('fs');
 const path = require('path')
+let imagenes=[]
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('formImg');
 });
-router.post('/upload',(req,res)=>{
+router.post('/upload',(req,res,next)=>{
   const form = formidable({uploadDir:'upload'});
   form.parse(req,(err,fields,files)=>{
     if(err){
@@ -15,10 +16,27 @@ router.post('/upload',(req,res)=>{
       return;
     }
     let ext = path.extname(files.archivo.originalFilename);
-   
-   //fs.renameSync(files.archivo.originalFilename,`${path.join(__dirname,'../public/images/')}${fields.title}-${files.archivo.newFilename}${ext.toLowerCase()}`)
-    res.json({fields,files})
+    if(ext==='.png'){
+      console.log('es un png');
+   //la ruta del archivo hay que colocarla completa
+   fs.renameSync(`${path.join(__dirname,'../upload/')}${files.archivo.newFilename}`,
+                 `${path.join(__dirname,'../public/images/')}${fields.title}-${files.archivo.newFilename}${ext.toLowerCase()}`)
+    imagenes=[...imagenes,`${path.join(__dirname,'../public/images/')}${fields.title}-${files.archivo.newFilename}${ext.toLowerCase()}`]
+                 return res.json('archivo subido')
+  }else{
+    return res.send('el archivo no es una imagen')
+  }
   })
+})
+router.get('/deleteAll',(req,res)=>{
+
+  imagenes.forEach(im=>{
+    if(fs.existsSync(im)){
+    fs.unlinkSync(im)
+  }
+  
+  });
+  res.send('eliminado todo')
 })
 
 module.exports = router;
---
+++ trabajo_final/routes/hotelesRoutes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const hotelController = require('../controllers/hotelController')
+
+
+router.get('/', hotelController.list);//read
+router.get('/add', hotelController.add);
+router.post('/add', hotelController.addPost);//create
+router.get('/edit/:id',hotelController.edit)
+router.put('/edit',hotelController.editPost)//Update
+router.delete('/delete',hotelController.delete)//delete
+module.exports = router;
